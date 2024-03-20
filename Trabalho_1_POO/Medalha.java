77efa2a3d97cde3b69223f31bd714dd1e9db61b8
RafaelSpumberg <150760513+RafaelSpumberg@users.noreply.github.com>
2024-03-20 15:37:38 -0300

    Update Medalha.java

--- Trabalho_1_POO/Medalha.java
+++ Trabalho_1_POO/Medalha.java
@@ -1,19 +1,40 @@
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Medalha {
 
 	private int codigo;
-
 	private int tipo;
-
 	private boolean individual;
-
 	private String modalidade;
-
 	private Collection<Atleta> atleta;
 
-	public void adicionaAtleta(Atleta atleta) {
+	public Medalha (int codigo, int tipo, boolean individual, String modalidade){
+		this.codigo=codigo;
+		this.tipo=tipo;
+		this.individual=individual;
+		this.modalidade=modalidade;
+		this.atleta = new ArrayList<>();
+	}
 
+	public int getCodigo(){
+		return codigo;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public boolean isIndividual() {
+		return individual;
+	}
+
+	public String getModalidade(){
+		return modalidade;
+	}
+
+	public void adicionaAtleta(Atleta atleta) {
+		this.atleta.add(atleta);
 	}
 
 }
